f3e71aa5e64edcc7a2b0e6d2497106c2c847feaf
Lisleny <lisleny.silva@dbserver.com.br>
2025-08-13 11:59:40 -0300

    feat: Implement JutsuNaoEncontradoException and exception handler

--- src/main/java/com/crud/naruto/exception/exceptionhandler/ExceptionHandler.java
+++ src/main/java/com/crud/naruto/exception/exceptionhandler/ExceptionHandler.java
@@ -1,4 +1,22 @@
 package com.crud.naruto.exception.exceptionhandler;
 
+import com.crud.naruto.exception.JutsuNaoEncontradoException;
+import com.crud.naruto.exception.PersonagemNaoEncontradoException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
 public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler
+    public ResponseEntity<String> personagemNaoEncontradoExceptionHandler(PersonagemNaoEncontradoException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler
+    public ResponseEntity<String> jutsuNaoEncontradoExceptionHandler(JutsuNaoEncontradoException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
 }

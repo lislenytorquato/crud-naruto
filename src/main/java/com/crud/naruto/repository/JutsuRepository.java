9ea144c16d542437466a1d2627aadfbdd7d06cb3
lislenytorquato <lislenytorquato19@gmail.com>
2025-09-15 16:35:35 -0300

    refactor: adding mongo repository

--- src/main/java/com/crud/naruto/repository/JutsuRepository.java
+++ src/main/java/com/crud/naruto/repository/JutsuRepository.java
@@ -3,5 +3,5 @@ package com.crud.naruto.repository;
 import com.crud.naruto.model.Jutsu;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface JutsuRepository extends MongoRepository<Jutsu,Long> {
+public interface JutsuRepository extends MongoRepository<Jutsu,String> {
 }
--- src/main/java/com/crud/naruto/repository/PersonagemRepository.java
+++ src/main/java/com/crud/naruto/repository/PersonagemRepository.java
@@ -3,5 +3,5 @@ package com.crud.naruto.repository;
 import com.crud.naruto.model.Personagem;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface PersonagemRepository extends MongoRepository<Personagem,Long> {
+public interface PersonagemRepository extends MongoRepository<Personagem,String> {
 }
